cdf15d0ffbdc506c72d43bcee0e89a1b9974d1b2
Khush <khushboosahubr.21@gmail.com>
2021-05-19 23:50:22 +0530

    Initial commit

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,18 @@
+var name = "Khushboo";
+
+var courseName ="JS REACT NODE";
+
+var isLoggedIn = false;
+
+var loggedCount = 5 ;
+
+console.log(loggedCount);
+console.log("loggedCount");
+
+var paymentMode;
+
+console.log(paymentMode);
+
+paymentMode ="Credit Card";
+
+console.log(paymentMode);
